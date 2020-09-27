a677496a8760fc8c2973fb1ad01309c52752f925
Ali <mohammadali18997@gmail.com>
2020-09-27 07:25:07 -0400

    Client Server Project

--- CNTEST/src/Testing/PrintTest.java
+++
@@ -1,7 +0,0 @@
-package Testing;
-import java.io.*;
-public class PrintTest {
-	 public static void main(String[] args) {
-	        System.out.println("This will be printed");
-	    }
-}
---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String args[])
+    {
+        Client client = new Client();
+        client.run();
+        System.out.println("asd");
+    }
+}
